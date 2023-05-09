5b0c0a90d1ec4628fd0ee8232fa699de7e256c20
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-05-09 13:57:53 -0700

    Add files via upload

---
+++ block.js
@@ -0,0 +1,4 @@
+if (window.location.href.indexOf("cat") !== -1 && window.location.href.indexOf("blender") !== -1) {
+    window.location.replace("https://www.google.com/search?q=blocked+page+error");
+  }
+  
