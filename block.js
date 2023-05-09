1b839a7b4e26ba1d0f6298b67275bf56cda9575e
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-05-09 14:07:40 -0700

    Add files via upload

--- block.js
+++ block.js
@@ -1,4 +1,4 @@
-if (window.location.href.indexOf("cat") !== -1 && window.location.href.indexOf("blender") !== -1) {
-    window.location.replace("https://www.google.com/search?q=blocked+page+error");
+if (window.location.href.indexOf("cat") !== -1 && window.location.href.indexOf("blender") !== -1 && window.location.href.indexOf("github.com/HttpAnimation/Cat-blender-Blocker/tree/main") === -1) {
+    window.location.replace(browser.extension.getURL("index.html"));
   }
   
